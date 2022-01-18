6c84cc996162db7849909bd5d06f9f6a4eea286e
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2022-01-18 15:53:57 -0300

    test[#2]: add test that product quantity update

--- src/__tests__/hooks/hookUseCart.spec.tsx
+++ src/__tests__/hooks/hookUseCart.spec.tsx
@@ -123,13 +123,13 @@ describe("Hook useCard", () => {
   });
 
   it("Aumentar a quantidade de um produto quando adicionar um produto que já existe no carrinho", async () => {
-    const productId = 1;
+    const idProduct = 1;
 
-    apiMock.onGet(`stock/${productId}`).reply(200, {
+    apiMock.onGet(`stock/${idProduct}`).reply(200, {
       id: 1,
       amount: 23,
     });
-    apiMock.onGet(`products/${productId}`).reply(200, {
+    apiMock.onGet(`products/${idProduct}`).reply(200, {
       id: 1,
       image:
         "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRx_7PC-kVaK0xOeqC969UaI39qcfpAJxVz6Csnh4S-28nr4yY17hBls8IF2e7GYvNFL4ckRgwFzEL7Idxlm-HV6yP7I1u4oxKUy6IafWdo&usqp=CAE",
@@ -142,7 +142,7 @@ describe("Hook useCard", () => {
     });
 
     act(() => {
-      result.current.addProduct(productId);
+      result.current.addProduct(idProduct);
     });
 
     await waitForNextUpdate({ timeout: 200 });
@@ -174,14 +174,14 @@ describe("Hook useCard", () => {
   });
 
   it("Teste de remover produto", () => {
-    const productId = 1;
+    const idProduct = 1;
 
     const { result } = renderHook(useCart, {
       wrapper: CartProvider,
     });
 
     act(() => {
-      result.current.removeProduct(productId);
+      result.current.removeProduct(idProduct);
     });
 
     expect(result.current.cart).toEqual(
@@ -189,6 +189,51 @@ describe("Hook useCard", () => {
         {
           id: 2,
           amount: 40,
+          image:
+            "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS7xhvzwYuN6MHS5cvjPus-IYDlNe1eRpatiyB_zqjvEye22XA2vTagDsY8w7b9LskMSLNJL9EwWzGK3x80MCmTTLwde0ClZy33_Hpivqo&usqp=CAE",
+          price: 23.55,
+          title: "Batom nude cobertura total",
+        },
+      ])
+    );
+    expect(setItemLocalStorage).toHaveBeenCalledWith(
+      "@SiteCompras:cart",
+      JSON.stringify(result.current.cart)
+    );
+  });
+
+  it("Teste para atualizar a quantidade de um produto do estoque", async () => {
+    const idProduct = 2;
+
+   apiMock.onGet(`stock/${idProduct}`).reply(200, {
+      id: 2,
+      amount: 40,
+    }); 
+
+    const { result, waitForNextUpdate } = renderHook(useCart, {
+      wrapper: CartProvider,
+    });
+
+    act(() => {
+      result.current.updateProductAmount({ amount: 4, productId: idProduct });
+    });
+
+    await waitForNextUpdate({ timeout: 200 });
+
+
+    expect(result.current.cart).toEqual(
+      expect.arrayContaining([
+        {
+          id: 1,
+          amount: 22,
+          image:
+            "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRx_7PC-kVaK0xOeqC969UaI39qcfpAJxVz6Csnh4S-28nr4yY17hBls8IF2e7GYvNFL4ckRgwFzEL7Idxlm-HV6yP7I1u4oxKUy6IafWdo&usqp=CAE",
+          price: 44.55,
+          title: "Batom Matte - Descontraída - Dailus, Nude, Descontraida",
+        },
+        {
+          id: 2,
+          amount: 4,
           image:
             "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS7xhvzwYuN6MHS5cvjPus-IYDlNe1eRpatiyB_zqjvEye22XA2vTagDsY8w7b9LskMSLNJL9EwWzGK3x80MCmTTLwde0ClZy33_Hpivqo&usqp=CAE",
           price: 23.55,
