399c67029fcc9483dfd6832d09d37cac5a6bcd65
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2022-01-20 18:06:28 -0300

    test[#4]: add test that adds a product to the cart

--- src/__tests__/pages/home.spec.tsx
+++ src/__tests__/pages/home.spec.tsx
@@ -3,7 +3,7 @@ import AxiosMock from "axios-mock-adapter";
 import Home from "../../pages/Home";
 import { useCart } from "../../hooks/useCart";
 
-import { waitFor, render } from "@testing-library/react";
+import { waitFor, render, fireEvent } from "@testing-library/react";
 import { api } from "../../services/api";
 
 const apiMock = new AxiosMock(api);
@@ -43,21 +43,21 @@ describe("Testes da página Home", () => {
     mockedUseCartHook.mockReturnValue({
       cart: [
         {
-            id: 1,
-            amount: 22,
-            image:
-              "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRx_7PC-kVaK0xOeqC969UaI39qcfpAJxVz6Csnh4S-28nr4yY17hBls8IF2e7GYvNFL4ckRgwFzEL7Idxlm-HV6yP7I1u4oxKUy6IafWdo&usqp=CAE",
-            price: 44.55,
-            title: "Batom Matte - Descontraída - Dailus, Nude, Descontraida",
-          },
-          {
-            id: 2,
-            amount: 44,
-            image:
-              "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS7xhvzwYuN6MHS5cvjPus-IYDlNe1eRpatiyB_zqjvEye22XA2vTagDsY8w7b9LskMSLNJL9EwWzGK3x80MCmTTLwde0ClZy33_Hpivqo&usqp=CAE",
-            price: 23.55,
-            title: "Batom nude cobertura total",
-          },
+          id: 1,
+          amount: 22,
+          image:
+            "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRx_7PC-kVaK0xOeqC969UaI39qcfpAJxVz6Csnh4S-28nr4yY17hBls8IF2e7GYvNFL4ckRgwFzEL7Idxlm-HV6yP7I1u4oxKUy6IafWdo&usqp=CAE",
+          price: 44.55,
+          title: "Batom Matte - Descontraída - Dailus, Nude, Descontraida",
+        },
+        {
+          id: 2,
+          amount: 44,
+          image:
+            "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS7xhvzwYuN6MHS5cvjPus-IYDlNe1eRpatiyB_zqjvEye22XA2vTagDsY8w7b9LskMSLNJL9EwWzGK3x80MCmTTLwde0ClZy33_Hpivqo&usqp=CAE",
+          price: 23.55,
+          title: "Batom nude cobertura total",
+        },
       ],
       addProduct: mockedAddProduct,
     });
@@ -79,5 +79,37 @@ describe("Testes da página Home", () => {
     expect(firstProductCartQuantity).toHaveTextContent("22");
     expect(secondProductCartQuantity).toHaveTextContent("44");
     expect(thirdProductCartQuantity).toHaveTextContent("0");
+  });
+
+  it("Teste para adicionar um produto ao carrinho", async () => {
+    const { getAllByTestId, rerender } = render(<Home />);
+
+    await waitFor(() => getAllByTestId("add-product-button"), {
+      timeout: 200,
+    });
+
+    const [addFirstProduct] = getAllByTestId("add-product-button");
+
+    fireEvent.click(addFirstProduct);
+
+    expect(mockedAddProduct).toHaveBeenCalledWith(1);
+
+    mockedUseCartHook.mockReturnValueOnce({
+      cart: [
+        {
+          id: 1,
+          amount: 22,
+          image:
+            "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRx_7PC-kVaK0xOeqC969UaI39qcfpAJxVz6Csnh4S-28nr4yY17hBls8IF2e7GYvNFL4ckRgwFzEL7Idxlm-HV6yP7I1u4oxKUy6IafWdo&usqp=CAE",
+          price: 44.55,
+          title: "Batom Matte - Descontraída - Dailus, Nude, Descontraida",
+        },
+      ],
+    });
+
+    rerender(<Home />);
+
+    const [firstProductCartQuantity] = getAllByTestId("cart-product-quantity");
+    expect(firstProductCartQuantity).toHaveTextContent("22");
   });
 });
